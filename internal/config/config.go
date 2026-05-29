091a89014f7cd352747015462c603ab316b91fc1
y.hatano <y.hatano.github@gmail.com>
2026-05-29 16:30:11 +0900

    fix: config param (pass -> password)

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,11 +6,11 @@ type Config struct {
 	Host      string `yaml:"host"`
 	Port      int    `yaml:"port"`
 	User      string `yaml:"user"`
-	Pass      string `yaml:"pass"`
+	Pass      string `yaml:"password"`
 	SSHHost   string `yaml:"ssh_host"`
 	SSHPort   int    `yaml:"ssh_port"`
 	SSHUser   string `yaml:"ssh_user"`
-	SSHPass   string `yaml:"ssh_pass"`
+	SSHPass   string `yaml:"ssh_password"`
 	SSHKey    string `yaml:"ssh_key"`
 	Charset   string `yaml:"charset"`
 	Tee       string `yaml:"tee"`
