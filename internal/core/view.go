6aafa6ce15f44c5a11376634e2f5c87a2918b82d
y.hatano <y.hatano.github@gmail.com>
2026-06-01 12:18:04 +0900

    refactor: remove unnecessary code

--- internal/core/view.go
+++ internal/core/view.go
@@ -2,27 +2,9 @@ package core
 
 import (
 	"myt/internal/render"
-	"strings"
 
 	"github.com/charmbracelet/lipgloss"
 )
-
-func truncateText(name string, maxWidth int) string {
-	if lipgloss.Width(name) <= maxWidth {
-		return name
-	}
-	var w int
-	var sb strings.Builder
-	for _, r := range name {
-		rw := lipgloss.Width(string(r))
-		if w+rw > maxWidth-2 { // ".."
-			break
-		}
-		sb.WriteRune(r)
-		w += rw
-	}
-	return sb.String() + ".."
-}
 
 func (m *Model) View() string {
 	var s render.MyStringBuilder
